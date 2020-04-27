77cb9fa0403421a1aecd852a359aebb06d292578
Warren Thompson <warren.thompson@b3groupinc.com>
2020-04-27 09:05:59 -0700

    Adding student component for workshop

---
+++ src/Student1/Student1.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as styles from './Student1.scss';
+
+interface Child {
+    name?: string;
+}
+
+export class Student1 extends React.Component<Child, {}> {
+  render() {
+    return (
+        <div className={styles.myName}>
+        <p>My name is: {this.props.name}</p>
+        </div>
+    );
+  }
+}
+
--- stories/jujuspritecomponent.stories.tsx
+++ stories/jujuspritecomponent.stories.tsx
@@ -2,16 +2,32 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { Julian } from '../src/jujuspritecomponent';
+import { Student1 } from '../src/Student1/Student1';
+
+storiesOf('Student1', module)
+  .addDecorator(withInfo)
+  .add(
+    'Student 1\'s component with no name passed',
+    () => <Student1 />,
+    { info: { header: true, inline: true } }
+  )
+  .add(
+    'Student 1\'s name property and rendering', 
+    () => <Student1 name="Student1"></Student1>,
+    { info: { header: true, inline: true } }
+  )
+;
 
 storiesOf('Julian', module)
   .addDecorator(withInfo)
   .add(
     'No name rendered',   
     () => <Julian />,
-    { info: { test: "dodes this work?", header: true, inline: true } }
+    { info: { header: true, inline: true } }
   )
   .add(
     'Passing name property and rendering', 
     () => <Julian name="Jujuman"></Julian>,
-    { info: { inline: true } }
-  );
+    { info: { header: true, inline: true } }
+  )
+;
