84f11b7201b53d8777fccc9a628fe37862bec6e6
Warren Thompson <warrenjthompson@gmail.com>
2020-06-27 18:19:31 -0700

    Adding step 3

---
+++ src/PuppySpriteStep3/PuppySprite3.scss.d.ts
@@ -0,0 +1,3 @@
+export const character: string;
+export const walkdog: string;
+export const flipped: string;
---
+++ src/PuppySpriteStep3/PuppySprite3.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as styles from './PuppySprite3.scss';
+
+interface SpriteDetailsProp {
+  flipped?: boolean;
+}
+
+export class PuppySpriteStep2 extends React.Component<SpriteDetailsProp, SpriteDetailsProp> {
+
+  constructor(props: SpriteDetailsProp) {
+    super(props);
+
+    this.state = {
+      flipped: this.props.flipped,
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.character + " " + (this.state.flipped ? styles.flipped:"")}></div>
+    );
+  }
+}
+
