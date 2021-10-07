805187fd59ab5738c136e1358ffea090fa9967f6
wtvamp <warrenjthompson@gmail.com>
2021-10-07 12:41:03 -0700

    Updated sass loaders and typings to modern versions

--- src/jujuspritecomponent.scss.d.ts
+++ src/jujuspritecomponent.scss.d.ts
@@ -1,1 +1,12 @@
-export const myName: string;
+declare namespace JujuspritecomponentScssNamespace {
+  export interface IJujuspritecomponentScss {
+    myName: string;
+  }
+}
+
+declare const JujuspritecomponentScssModule: JujuspritecomponentScssNamespace.IJujuspritecomponentScss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: JujuspritecomponentScssNamespace.IJujuspritecomponentScss;
+};
+
+export = JujuspritecomponentScssModule;
